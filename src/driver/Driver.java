0b3449e33c24e45121a29a5d74ce7925eaaa3059
rs280 <rishabhsahoo1@gmail.com>
2022-02-03 22:08:53 -0800

    adding scalability

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -1,120 +1,47 @@
+//============================================================================
+// Name        : Assignment 4
+// Author      : Branden Lee
+// Date        : 4/24/2018
+// Description : Driver class for the KBB website application
+//============================================================================
 
 package driver;
 
-// import model.Automobile;
+//import model.Automobile;
 import adapter.*;
+import scale.EditOptions;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		CreateAuto buildAutoInterface = new CreateAuto();
+		/* This test driver is similar to Hello.java */
+		BuildAuto buildAutoInterface = new BuildAuto();
 		buildAutoInterface.init(); // only call this once
 		// Build Automobile Object from a file.
-		String FordZTW2021AutomobileKey = buildAutoInterface.buildAuto("FordZTW2021.txt");
-		if (FordZTW2021AutomobileKey != null) {
+		String FordZTWAutomobileKey = buildAutoInterface.buildAuto("FordZTW2021.txt");
+		
+		if (FordZTWAutomobileKey != null) {
 			// Print attributes before serialization
-			buildAutoInterface.printAuto(FordZTW2021AutomobileKey);
-			// update an options set's name
-			buildAutoInterface.updateOptionSetName(FordZTW2021AutomobileKey, "Color", "Colors");
-			// update an options set's option price
-			buildAutoInterface.updateOptionPrice(FordZTW2021AutomobileKey, "Power Moonroof", "present", 595);
-			// choose a transmission option
-			buildAutoInterface.setOptionChoice(FordZTW2021AutomobileKey, "Brakes/Traction Control", "ABS with Advance Track");
-			// Serialize the object
-			buildAutoInterface.serialize(FordZTW2021AutomobileKey, "FordZTW2021.data");
+			buildAutoInterface.printAuto(FordZTWAutomobileKey);
+			/*
+			 * We will start a bunch of operation threads to demonstrate multi-threading
+			 * Some operations depend on the order and silently throw an exception that will be logged
+			 */
+			String input[] = { FordZTWAutomobileKey, "Color", "French Blue Clearcoat Metallic",
+				"Cool Cali Blue" };
+			String input2[] = { FordZTWAutomobileKey, "Color", "French Blue Clearcoat Metallic",
+				"Sunshine Hawaiian Luau Gold" };
+			String input3[] = { FordZTWAutomobileKey, "Color", "Colors"};
+			String input4[] = { FordZTWAutomobileKey, "Color", "French Blue Clearcoat Metallic"};
+			buildAutoInterface.operation(0, input); // Updates option name
+			buildAutoInterface.operation(0, input2); // Updates option name
+			buildAutoInterface.operation(1, input3); // Updates option set name
+			buildAutoInterface.operation(3, input4); // set color choice
+			
 		} else {
 			System.out.println("Could not build automobile");
 		}
-		// Deserialize the object and read it into memory.
-		String FordZTW2021AutomobileKey2 = buildAutoInterface.deserialize("FordZTW2021.data");
-		// Print new attributes
-		if (FordZTW2021AutomobileKey2 != null) {
-			// Print attributes after deserialization
-			buildAutoInterface.printAuto(FordZTW2021AutomobileKey2);
-			// Print transmission choice
-			String optionName = buildAutoInterface.getOptionChoice(FordZTW2021AutomobileKey, "Transmission");
-			if (optionName != null) {
-				System.out.println("\nTransmission choices: " + optionName);
-			}
-			// Print transmission choice price
-			Double optionPrice = buildAutoInterface.getOptionChoicePrice(FordZTW2021AutomobileKey, "Transmission");
-			if (optionName != null) {
-				System.out.println("\nTransmission choice price: $" + optionPrice);
-			}
-		} else {
-			System.out.println("could not deserialize automobile");
-		}
-
-		//Testing with TeslaModelY
-		String TeslaModelY2022AutomobileKey = buildAutoInterface.buildAuto("TeslaModelY2022.txt");
-		if (TeslaModelY2022AutomobileKey != null) {
-			// Print attributes before serialization
-			buildAutoInterface.printAuto(TeslaModelY2022AutomobileKey);
-			// update an options set's name
-			buildAutoInterface.updateOptionSetName(TeslaModelY2022AutomobileKey, "Color", "Colors");
-			// update an options set's option price
-			buildAutoInterface.updateOptionPrice(TeslaModelY2022AutomobileKey, "Trim", "Brushed Aluminum trim", 2000);
-			// choose a transmission option
-			buildAutoInterface.setOptionChoice(TeslaModelY2022AutomobileKey, "Tier", "Full Self-Driving Capability");
-			// Serialize the object
-			buildAutoInterface.serialize(TeslaModelY2022AutomobileKey, "TeslaModelY2022.data");
-		} else {
-			System.out.println("Could not build automobile");
-		}
-		// Deserialize the object and read it into memory.
-		String TeslaModelY2022AutomobileKey2 = buildAutoInterface.deserialize("TeslaModelY.data");
-		// Print new attributes
-		if (TeslaModelY2022AutomobileKey2 != null) {
-			// Print attributes after deserialization
-			buildAutoInterface.printAuto(TeslaModelY2022AutomobileKey2);
-			// Print transmission choice
-			String optionName = buildAutoInterface.getOptionChoice(TeslaModelY2022AutomobileKey, "Colors");
-			if (optionName != null) {
-				System.out.println("\n Color choice: " + optionName);
-			}
-			// Print transmission choice price
-			Double optionPrice = buildAutoInterface.getOptionChoicePrice(TeslaModelY2022AutomobileKey, "Colors");
-			if (optionName != null) {
-				System.out.println("\n Color choice price: $" + optionPrice);
-			}
-		} else {
-			System.out.println("could not deserialize automobile");
-		}
-		// Testing with Camry 2020
-		String ToyotaCamry2020AutomobileKey = buildAutoInterface.buildAuto("Camry2020.txt");
-		if (ToyotaCamry2020AutomobileKey != null) {
-			// Print attributes before serialization
-			buildAutoInterface.printAuto(ToyotaCamry2020AutomobileKey);
-			// update an options set's name
-			buildAutoInterface.updateOptionSetName(ToyotaCamry2020AutomobileKey, "Color", "Colors");
-			// update an options set's option price
-			buildAutoInterface.updateOptionPrice(ToyotaCamry2020AutomobileKey, "Accesories", "Cargo Tray", 59);
-			// choose a transmission option
-			buildAutoInterface.setOptionChoice(ToyotaCamry2020AutomobileKey, "Engine", "All-Wheel Drive");
-			// Serialize the object
-			buildAutoInterface.serialize(ToyotaCamry2020AutomobileKey, "ToyotaCamry2020.data");
-		} else {
-			System.out.println("Could not build automobile");
-		}
-		// Deserialize the object and read it into memory.
-		String ToyotaCamry2020AutomobileKey2 = buildAutoInterface.deserialize("ToyotaCamry2020.data");
-		// Print new attributes
-		if (ToyotaCamry2020AutomobileKey2 != null) {
-			// Print attributes after deserialization
-			buildAutoInterface.printAuto(ToyotaCamry2020AutomobileKey2);
-			// Print transmission choice
-			String optionName = buildAutoInterface.getOptionChoice(ToyotaCamry2020AutomobileKey, "Colors");
-			if (optionName != null) {
-				System.out.println("\n Color choice: " + optionName);
-			}
-			// Print transmission choice price
-			Double optionPrice = buildAutoInterface.getOptionChoicePrice(ToyotaCamry2020AutomobileKey, "Colors");
-			if (optionName != null) {
-				System.out.println("\n Color choice price: $" + optionPrice);
-			}
-		} else {
-			System.out.println("could not deserialize automobile");
-		}
+		
 	}
 
 }
